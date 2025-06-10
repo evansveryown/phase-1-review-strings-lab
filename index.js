675cdd79ac66d8c3529d07f08c66ba1a61381900
Cassy Omondi <cksparks07@gmail.com>
2025-06-10 18:07:02 +0300

    Complete  Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define currentUser
+const currentUser = 'Grace Hopper';
+
+// Create a welcome message with template literals and exclamation mark
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Convert the welcome message to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting using only the first initial of the currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
